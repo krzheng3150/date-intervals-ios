cbe1ae9b2d63c67e8d0ee3fd407e839d850397bd
Nivek <kevin_ruoyu_zheng@hotmail.com>
2017-05-30 13:26:17 -0400

    Initial Commit

---
+++ DateIntervals/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DateIntervals
+//
+//  Created by ZhengFamily on 30/05/2017.
+//  Copyright © 2017 UnnamedGreenCompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
