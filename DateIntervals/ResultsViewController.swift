6178216b24af680858ee0e26e2ed7bf3bccc163c
Nivek <kevin_ruoyu_zheng@hotmail.com>
2017-06-02 11:41:44 -0400

    added outlet functions to each button, and logic to show an alert if the number of intervals is too big

--- DateIntervals/ResultsViewController.swift
+++ DateIntervals/ResultsViewController.swift
@@ -39,6 +39,30 @@ class ResultsViewController: UIViewController {
         
     }
     
+    @IBAction func onViewResults(_ sender: UIButton) {
+        if (numIntervals > 100) {
+            alert(title: "Results Too Big", message: "Unfortunately, because there are more than 100 intervals, they will not be displayed. Please download or email them.")
+        }
+        else {
+            
+        }
+    }
+    
+    @IBAction func onDownload(_ sender: UIButton) {
+    }
+    
+    @IBAction func onSendEmail(_ sender: UIButton) {
+    }
+    
+    @IBAction func onBackClick(_ sender: UIButton) {
+    }
+    
+    func alert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
